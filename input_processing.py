c14a1a0090d2d491e7478953f90b4cbb57958a85
ctcastle <conner.castle@ucalgary.ca>
2025-05-13 10:14:05 -0600

    wrote desired outline in file

--- input_processing.py
+++ input_processing.py
@@ -1,5 +1,5 @@
 # input_processing.py
-# <YOUR NAME>, ENSF 692 Spring 2025
+# Conner Castle, ENSF 692 Spring 2025
 # A terminal-based program for processing computer vision changes detected by a car.
 # Detailed specifications are provided via the Assignment 2 README file.
 # You must include the code provided below but you may delete the instructional comments.
@@ -20,9 +20,9 @@ class Sensor:
     def __init__(self):
         pass
 
-    # Replace these comments with your function commenting
-    def update_status(): # You may decide how to implement the arguments for this function
-        pass
+    # Takes a "measurement" of the world and outputs the result 
+    def update_status(): # You may decide how to implement the arguments for this function 
+        pass 
 
 
 
@@ -36,6 +36,20 @@ def print_message(sensor):
 # Complete the main function below
 def main():
     print("\n***ENSF 692 Car Vision Detector Processing Program***\n")
+    runFlag = True 
+    while runFlag:
+        sensorStatus = input("Update sensor:\n 1. Update stoplight detection\n 2. Update pedestrian detection\n 3. Update vehicle detection\n 4. Q to quit\n")
+        runFlag = (menuInput.lower() != 'q')
+
+# Two ways I could see doing this: 
+# Either you should have Sensor request updates that the main command provides/ so like pretending that the main IS the sensor getting data 
+# Or you have the main command request updates from the sensor/ pretending that you are requesting data FROM the sensor 
+# Basically this is the difference between a publisher, or master/slave communication style 
+
+# I think I want to do this by having all the sensor logic in the sensor so: 
+# So basically sensor.update will "measure" the state of the world 
+# But then have a separate logic that says what to do with the updated status 
+# This way the sensor just measures things but the main function "brain" decides what to do with those things 
 
 
 
